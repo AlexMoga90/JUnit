12d92297311bf888a472d2fc923c76ce41879067
OtiliaMihet <160628424+OtiliaMihet@users.noreply.github.com>
2024-08-23 20:41:56 +0300

    Refactorizare

--- src/main/java/pl/coderslab/tdd/tdd/Factors.java
+++ src/main/java/pl/coderslab/tdd/tdd/Factors.java
@@ -1,19 +1,33 @@
 package pl.coderslab.tdd.tdd;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Factors {
-    public static List<Integer> generatePrimeFactors(int n){
+    public static List<Integer> generatePrimeFactors(int n) {
 
-        return IntStream.rangeClosed(2, n)
-                .filter(x -> isPrime(x)).boxed()
-                .collect(Collectors.toList());
-    }
-    private static boolean isPrime(int number) {
-        return IntStream.rangeClosed(2, (int) (Math.sqrt(number)))
-                .allMatch(n -> number % n != 0);
+        List<Integer> factors = new ArrayList<>();
+        // Add the number of 2s that divide n to the list
+        while (n % 2 == 0) {
+            factors.add(2);
+            n /= 2;
+        }
+        // n must be odd at this point, so a skip of 2 is done (i.e., i = i + 2)
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            // While i divides n, add i to the list and divide
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        // This condition is to check if n is a prime number greater than 2
+        if (n > 2) {
+            factors.add(n);
+        }
+        return factors;
+
     }
 }
--- src/test/java/pl/coderslab/tdd/tdd/FactorsTest.java
+++ src/test/java/pl/coderslab/tdd/tdd/FactorsTest.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class FactorsTest {
     @Test
     public void testFactors() {
-       List<Integer> factoriPrimi = Factors.generatePrimeFactors(30);
-        assertNotEquals(0,factoriPrimi.size());
+        List<Integer> factoriPrimi = Factors.generatePrimeFactors(10);
+        assertEquals(2, factoriPrimi.size());
+        assertTrue(factoriPrimi.containsAll(List.of(2, 5)));
     }
 }
